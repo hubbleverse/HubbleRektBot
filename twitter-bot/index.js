1abe197235c1cbfd84a9d3d89d292970bf301460
devhdap <83585711+devhdap@users.noreply.github.com>
2021-10-27 08:40:34 +0530

    Ethers.commit 1

--- twitter-bot/index.js
+++ twitter-bot/index.js
@@ -1,52 +1,49 @@
 require("dotenv").config();
 const twit = require("./twit");
-const Web3 = require('web3');
+const { ethers } = require('ethers');
 const fs = require("fs");
 const path = require("path");
 const paramsPath = path.join(__dirname, "params.json");
 const keccak256 = require('keccak256');
-const web3 = new Web3(new Web3.providers.WebsocketProvider('wss://api.avax-test.network/ext/bc/C/ws'));
-var tweetInitializer={
-  status: `Hubble bot has been locally deployed. Expect nothing but chaos.`
-          
-}
-web3.eth.subscribe(
-  'logs',
-  {
-      address: '0xfe2239288Ab37b8bCCFb4ebD156463fb14EFC1e9',
-      topics: ['0x' + keccak256('PositionLiquidated(address,address,int256,uint256,int256)').toString('hex')]
-  },
-  function(error,result){
-      if(error)
-          console.log(error);
-  }
-)
-.on("connected",function(subscriptionId){
-  console.log(subscriptionId);
-  twit.post('statuses/update',tweetInitializer,tweeted);
-function tweeted(err,data,response){
-  if(err){
-    console.log(err);
-  }else{
-    console.log("Console : Connection successful !");
-  }
-}
-})
+const provider = new ethers.providers.WebSocketProvider('wss://api.avax-test.network/ext/bc/C/ws')
 
-.on("data",function(data){
-  let transactionHash=data.transactionHash;
-  web3.eth.getTransactionReceipt(transactionHash)
-     .then(console.log);
-  var tweet={
-    status : 'You have been liquidated good sir ! : https://cchain.explorer.avax-test.network/tx/'+transactionHash,
+const ammMap = {
+    '0x74583fEbc73B8cfEAD50107C49F868301699641E': 'ETH-PERP',
+    '0xCF9541901625fd348eDe299309597cB36f4e4328': 'BTC-PERP',
+    '0x352B0c6B3Ef762F4fA4C20CfecD14251236fD166': 'AVAX-PERP'
   }
-  twit.post('statuses/update',tweet,tweeted);
-function tweeted(err,data,response){
-  if(err){
-    console.log(err);
-  }else{
-    console.log("Console : Tweet Successful !");
+  
+  async function main() {
+    console.log('Console : Script is running now.')
+    const clearingHouseAbi = [
+      "event PositionLiquidated(address indexed trader, address indexed amm, int256 size, uint256 quoteAsset, int256 realizedPnl)",
+    ]
+    const MarginAbi = [
+      "event MarginAccountLiquidated(address indexed trader, uint indexed idx, uint seizeAmount, uint repayAmount)",
+    ]
+    const clearingHouse = new ethers.Contract('0xfe2239288Ab37b8bCCFb4ebD156463fb14EFC1e9', clearingHouseAbi, provider)
+    clearingHouse.on('PositionLiquidated', (trader, amm, size, _, __, event) => {
+        const Side = size.lt(0) ? 'LONG' : 'SHORT';
+        size = ethers.utils.formatEther(size.abs());
+        var tweet={
+            status : size +' ' +ammMap[amm] +' ' + Side +  ' position has been liquidated on @HubbleExchange : \n https://cchain.explorer.avax-test.network/tx/' + event.transactionHash,
+        }
+        twit.post('statuses/update',tweet,tweeted);
+        function tweeted(err,data,response){
+        if(err){
+        console.log(err);
+        }else{
+        console.log("Console : Tweet Successful !");
+        }
+      }
+      // tweet that "${size} ${ammMap[amm]} ${Side} position has been liquidated"
+      // e.g. 500 AVAX-PERP LONG position on @HubbleExchange has been liquidated! - provide tx link ${event.transactionHash}
+    })
+    const MarginLiq = new ethers.Contract('0x5977D567DD118D87062285a36a326A75dbdb3C6D',MarginAbi, provider)
+    MarginLiq.on('MarginAccountLiquidated',function(data){
+      console.log(data);
+      console.log('Analyze data ^')
+    })
   }
-}
-}
-  )
+
+main()
