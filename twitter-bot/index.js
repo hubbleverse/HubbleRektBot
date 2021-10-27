2deb8ab229b3866a5d62cc5476870e7bae963f1d
devhdap <devblixt@gmail.com>
2021-10-27 08:44:34 +0530

    Ethers.commit

--- twitter-bot/index.js
+++ twitter-bot/index.js
@@ -14,15 +14,20 @@ const ammMap = {
   }
   
   async function main() {
+    console.log('Console : Script is running now.')
     const clearingHouseAbi = [
       "event PositionLiquidated(address indexed trader, address indexed amm, int256 size, uint256 quoteAsset, int256 realizedPnl)",
     ]
+    const MarginAbi = [
+      "event MarginAccountLiquidated(address indexed trader, uint indexed idx, uint seizeAmount, uint repayAmount)",
+    ]
     const clearingHouse = new ethers.Contract('0xfe2239288Ab37b8bCCFb4ebD156463fb14EFC1e9', clearingHouseAbi, provider)
     clearingHouse.on('PositionLiquidated', (trader, amm, size, _, __, event) => {
+        const Side = size.lt(0) ? 'LONG' : 'SHORT';
+        size = ethers.utils.formatEther(size.abs());
         var tweet={
-            status : '${size} ${ammMap[amm]} ${Side} position has been liquidated on @HubbleExchange : \n https://cchain.explorer.avax-test.network/tx/${event.transactionHash}'
+            status : size +' ' +ammMap[amm] +' ' + Side +  ' position has been liquidated on @HubbleExchange : \n https://cchain.explorer.avax-test.network/tx/' + event.transactionHash,
         }
-        console.log(event);
         twit.post('statuses/update',tweet,tweeted);
         function tweeted(err,data,response){
         if(err){
@@ -33,6 +38,11 @@ const ammMap = {
       }
       // tweet that "${size} ${ammMap[amm]} ${Side} position has been liquidated"
       // e.g. 500 AVAX-PERP LONG position on @HubbleExchange has been liquidated! - provide tx link ${event.transactionHash}
+    })
+    const MarginLiq = new ethers.Contract('0x5977D567DD118D87062285a36a326A75dbdb3C6D',MarginAbi, provider)
+    MarginLiq.on('MarginAccountLiquidated',function(data){
+      console.log(data);
+      console.log('Analyze data ^')
     })
   }
 
